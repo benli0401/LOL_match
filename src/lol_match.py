15f43e4f22df34663a3c719c7ad80b04496b83dd
Ben_Li <csc2656@gmail.com>
2026-07-26 09:40:49 +0800

    fix: api key in code not in environment

--- src/lol_match.py
+++ src/lol_match.py
@@ -1,23 +1,41 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
 
 
-class LOLApi:
-    def __init__(self, RG_api_key):
-        self.header = {
-            "X-Riot-Token": RG_api_key,
+class RiotAccountClient:
+    def __init__(self, game_name, game_tag, headers):
+        self.game_name = game_name
+        self.game_tag = game_tag
+        self.headers = headers
+
+    def get_puuid(self):
+        url = (
+            f"https://asia.api.riotgames.com/"
+            f"riot/account/v1/accounts/by-riot-id/"
+            f"{self.game_name}/{self.game_tag}"
+        )
+
+        response = requests.get(
+            url=url,
+            headers=self.headers,
+            timeout=10
+        )
+
+        response.raise_for_status()
+
+        return response.json()["puuid"]
+
+load_dotenv()
+def main():
+    api_key = os.getenv("RGAPI_KEY")
+    api_headers = {
+            "X-Riot-Token": api_key,
             "Accept": "application/json"
         }
-
-    def get_puuid(self, game_name, game_tag):
-        api_url = f'https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{game_tag}'
-        result = requests.get(url=api_url, headers=self.header)
-        return result.text
-
-def main():
-    api_key = "RGAPI-6cd0599d-c667-4155-9571-86f14d646fbb"
-    api_infra = LOLApi(RG_api_key=api_key)
-    puuid = api_infra.get_puuid(game_name="Airhi", game_tag="0203")
+    
+    puuid = RiotAccountClient(game_name="Airhi", game_tag="0203", headers=api_headers).get_puuid()
     print(puuid)
 
 main()
